729f6b2be38028a8ebe4bad8237dcf8ef786a379
Pedro Henrique PHD <danesvara2002@gmail.com>
2022-10-04 18:19:28 -0300

    Versão 1.0
    Adicionado a possibilidade de comunicação entre um usuário com o servidor

---
+++ ChatCliente.py
@@ -0,0 +1,13 @@
+from socket import *
+
+servidor = "127.0.0.1"
+porta = 43210
+
+while True:
+    obj_socket = socket(AF_INET, SOCK_STREAM)
+    obj_socket.connect((servidor, porta))
+    msg = bytes(input("Sua mensagem: ", 'utf-8'))
+    obj_socket.send(msg)
+    resposta = obj_socket.recv(1024)
+    print("Resposta do Servidor: ", str(resposta)[2:-1])
+obj_socket.close()
