8237a38b359fbc9933cf9577957ed86203b882f1
Pedro Henrique PHD <danesvara2002@gmail.com>
2022-10-05 08:48:25 -0300

    Update ChatCliente.py

--- ChatCliente.py
+++ ChatCliente.py
@@ -6,7 +6,7 @@ porta = 43210
 while True:
     obj_socket = socket(AF_INET, SOCK_STREAM)
     obj_socket.connect((servidor, porta))
-    msg = bytes(input("Sua mensagem: ", 'utf-8'))
+    msg = bytes(input("Sua mensagem: "), 'utf-8')
     obj_socket.send(msg)
     resposta = obj_socket.recv(1024)
     print("Resposta do Servidor: ", str(resposta)[2:-1])
